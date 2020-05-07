40fd20373a4a57b992aff68e6921ecf5ae9d7b81
Peter Grainger <peter.grainger@arup.com>
2020-05-07 12:58:44 +0100

    copy pdf manuals to the installer

--- InstallerCreator/InstallerCreatorV1/installerCreator.ps1
+++ InstallerCreator/InstallerCreatorV1/installerCreator.ps1
@@ -67,6 +67,7 @@ try {
     $p.WaitForExit()
 
     Get-ChildItem -Filter *.chm -Path "oasys-combined\$project\help\output" -Recurse -Force | Copy-Item -Destination $targetDirectory
+    Get-ChildItem -Filter *.pdf -Path "oasys-combined\$project\help\output" -Recurse -Force | Copy-Item -Destination $targetDirectory
    }
 
    Write-Output "Copying DLLs"
