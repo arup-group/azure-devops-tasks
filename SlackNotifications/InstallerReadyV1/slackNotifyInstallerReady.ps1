1f8abd15e328671bf9c1ce053fca3a7d7ee243a6
peterjgrainger <peter.grainger@arup.com>
2020-06-09 13:13:28 +0100

    commited webhook address

--- SlackNotifications/InstallerReadyV1/slackNotifyInstallerReady.ps1
+++ SlackNotifications/InstallerReadyV1/slackNotifyInstallerReady.ps1
@@ -8,7 +8,8 @@ try {
   [string]$tagNumbersOnlyDot = Get-VstsTaskVariable -Name TAG_NUMBERS_ONLY_DOT
   [string]$underscoreSeparatedTag = Get-VstsTaskVariable -Name UNDERSCORE_SEPARATED_TAG
   [string]$buildId = Get-VstsTaskVariable -Name Build.BuildId
-  $uriSlack = "https://hooks.slack.com/services/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"
+  [string]$webhook = Get-VstsInput -Name webhook
+  $uriSlack = "https://hooks.slack.com/services/".$webhook
   $text = "Installers ready for $projectName version $tagNumbersOnlyDot -> https://github.com/arup-group/oasys-combined/releases"
   if($tagExists -eq "true"){$text = "Tag $underscoreSeparatedTag for slope exists. Update RC files. Build -> https://dev.azure.com/oasys-software/OASYS%20libraries/_build/results?buildId=$buildId"}
   $body = ConvertTo-Json @{text = $text}
